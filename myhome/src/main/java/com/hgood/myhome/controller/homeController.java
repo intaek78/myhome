e7e88c78d566c036e6dbe3b260eb4c0c2c29b710
I.T HWANG <int78@hanmail.net>
2020-11-28 17:27:13 +0900

    레이아웃 구성

---
+++ myhome/src/main/java/com/hgood/myhome/controller/homeController.java
@@ -0,0 +1,14 @@
+package com.hgood.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homeController {
+
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+
+}
